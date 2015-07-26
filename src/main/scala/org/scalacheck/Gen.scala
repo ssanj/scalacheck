6be6c0c9c2c5c4c0d82c52e96ac7a0ecdc5727c2
Rickard Nilsson <rickynils@gmail.com>
2015-07-26 19:09:44 +0200

    Use the RNG from the master branch

--- src/main/scala/org/scalacheck/Gen.scala
+++ src/main/scala/org/scalacheck/Gen.scala
@@ -9,6 +9,8 @@
 
 package org.scalacheck
 
+import rng.{Seed, Rng}
+
 sealed trait Gen[T] {
 
   import Gen._
@@ -21,32 +23,30 @@ sealed trait Gen[T] {
 
   def map[U](f: T => U): Gen[U] = mapResult(_.map(f))
 
+  def contramap(f: Parameters => Parameters): Gen[T] = gen(p => apply(f(p)))
+
   def filter(f: T => Boolean): Gen[T] = mapResult(_.filter(f))
 
-  def sampleResult(seed: Long, size: Int = 100): Result[T] =
-    apply(params(seed, size, { new scala.util.Random(_) }))
+  def sampleResult(seed: Int = 0, size: Int = 100): Result[T] =
+    apply(Parameters(Seed(seed), size))
 
-  def sampleResult: Result[T] = apply(params(0L, 100, { new scala.util.Random(_) }))
-
-  def sample: Option[T] = sampleResult.value
-
-  def sample(seed: Long, size: Int = 100): Option[T] =
+  def sample(seed: Int = 0, size: Int = 100): Option[T] =
     sampleResult(seed, size).value
 
   def flatMap[U](f: T => Gen[U]): Gen[U] = gen { p =>
-    val r1 = apply(p)
-    r1.value match {
-      case None => res(None, Nil, r1.next.map(_.flatMap(f)))
+    val rt = apply(p)
+    rt.value match {
+      case None => res(rt.nextSeed, None, Nil, rt.next.map(_.flatMap(f)))
       case Some(t) =>
-        val r2 = f(t).reseed.apply(p)
+        val ru = f(t).apply(p.copy(seed = rt.nextSeed))
         new Result[U] {
-          val params = p
-          val value = r2.value
-          lazy val shrink = r2.shrink ++ r1.shrink.flatMap { t =>
-            val r = f(t).reseed.apply(p)
+          val value = ru.value
+          val nextSeed = ru.nextSeed
+          lazy val shrink = ru.shrink ++ rt.shrink.flatMap { t =>
+            val r = f(t).apply(p.copy(seed = rt.nextSeed))
             r.value.toSeq ++ r.shrink
           }
-          lazy val next = (r1.next, r2.next) match {
+          lazy val next = (rt.next, ru.next) match {
             case (Some(gt),Some(gu)) => Some(oneOfGens(gt.flatMap(f), gu))
             case (None,gu) => gu
             case (gt,None) => gt.map(_.flatMap(f))
@@ -57,44 +57,36 @@ sealed trait Gen[T] {
 
   def repeatedly: Gen[T] = setNext(repeatedly)
 
-  def repeatedly(f: Gen[T] => Gen[T]): Gen[T] =
-    setNext(f(this)).mapNext(_.map(_.repeatedly(f)))
+  def resize(size: Int): Gen[T] = contramap(_.copy(size = size))
 
-  def setSize(size: Int): Gen[T] =
-    gen(p => apply(p.resize(size))).mapNext(_.map(_.setSize(size)))
+  def resize(f: Int => Int): Gen[T] = size flatMap (sz => resize(f(sz)))
 
-  def resize(f: Int => Int): Gen[T] = size flatMap (sz => setSize(f(sz)))
+  def setNext(g: => Gen[T]): Gen[T] = setNextOpt(Some(g))
 
-  def setSeed(newSeed: Long): Gen[T] = gen(p => apply(p.reseed(newSeed)))
-
-  def reseed: Gen[T] = gen(p => apply(p.reseed))
-
-  def setNext(g: => Gen[T]): Gen[T] = mapNext (_ => Some(g))
+  def setNextOpt(g: => Option[Gen[T]]): Gen[T] = mapNext(_ => g)
 
   def mapNext(f: Option[Gen[T]] => Option[Gen[T]]): Gen[T] = mapResult { r =>
-    res(r.value, r.shrink, f(r.next))
+    res(r.nextSeed, r.value, r.shrink, f(r.next))
   }
-
-  def terminate: Gen[T] = mapNext (_ => None)
 
   def withNext: Gen[(Option[T], Option[Gen[T]])] = mapResult { r =>
-    res(Some(r.value -> r.next), r.shrink.map(Some(_) -> r.next), None)
+    res(r.nextSeed, Some(r.value -> r.next), r.shrink.map(Some(_) -> r.next), None)
   }
 
-  def followedBy(g: Gen[T]): Gen[T] = withNext flatMap {
-    case (t,None) => fromOption(t) setNext g
-    case (t,Some(g0)) => fromOption(t) setNext g0.followedBy(g)
+  def followBy(g: => Gen[T]): Gen[T] = mapNext {
+    case None => Some(g)
+    case Some(g0) => Some(g0 followBy g)
   }
 
   def withShrink: Gen[(T, () => Seq[T])] = mapResult { r =>
-    res(r.value.map(_ -> (() => r.shrink)),
+    res(r.nextSeed, r.value.map(_ -> (() => r.shrink)),
       r.shrink.map(_ -> (() => r.shrink)), r.next.map(_.withShrink))
   }
 
   def noShrink: Gen[T] = setShrink(_ => Nil)
 
   def setShrink(f: T => Seq[T]): Gen[T] = mapResult { r =>
-    res(r.value, r.value.map(f).getOrElse(Nil), r.next)
+    res(r.nextSeed, r.value, r.value.map(f).getOrElse(Nil), r.next)
   }
 
   def unfold: Gen[Seq[T]] =
@@ -111,11 +103,10 @@ sealed trait Gen[T] {
 
   def iterate: Gen[Iterator[Result[T]]] = parameterized { p =>
     const(new Iterator[Result[T]] {
-      var nextGen: Option[Gen[T]] = Some(Gen.this)
-      def hasNext = nextGen.isDefined
+      var r = res(p.seed, None, Nil, Some(Gen.this))
+      def hasNext = r.next.isDefined
       def next() = {
-        val r = nextGen.get.apply(p)
-        nextGen = r.next
+        r = r.next.get.apply(p.copy(seed = r.nextSeed))
         r
       }
     })
@@ -125,36 +116,24 @@ sealed trait Gen[T] {
 
 object Gen {
 
-  sealed trait Parameters {
-    val seed: Long
-    val size: Int
-    def resize(newSize: Int): Parameters
-    def reseed: Parameters
-    def reseed(newSeed: Long): Parameters
-  }
+  case class Parameters(seed: Seed, size: Int)
 
-  def params(seed: Long, size: Int, rngf: Long => scala.util.Random): Parameters = {
-    case class P(seed: Long, size: Int) extends Parameters {
-      lazy val reseed = copy(seed = rngf(seed).nextLong)
-      def reseed(newSeed: Long) = copy(seed = newSeed)
-      def resize(newSize: Int) = copy(size = newSize)
-    }
-    P(seed, size)
-  }
-
-  sealed trait Result[T] {
+  sealed abstract class Result[T] {
     def value: Option[T]
+    def nextSeed: Seed
     def shrink: Seq[T]
     def next: Option[Gen[T]]
 
     def map[U](f: T => U): Result[U] = new Result[U] {
       def value = Result.this.value.map(f)
+      def nextSeed = Result.this.nextSeed
       def shrink = Result.this.shrink.map(f)
       def next = Result.this.next.map(_.map(f))
     }
 
     def filter(f: T => Boolean): Result[T] = new Result[T] {
       def value = Result.this.value.filter(f)
+      def nextSeed = Result.this.nextSeed
       def shrink = if(value.isDefined) Result.this.shrink else Nil
       def next = Result.this.next.map(_.filter(f))
     }
@@ -165,9 +144,10 @@ object Gen {
   }
 
   private def res[T](
-    v: Option[T], s: => Seq[T] = Nil, n: => Option[Gen[T]] = None
+    seed: Seed, v: Option[T], s: => Seq[T] = Nil, n: => Option[Gen[T]] = None
   ): Result[T] = new Result[T] {
     val value = v
+    val nextSeed = seed
     def shrink = s
     def next = n
   }
@@ -176,15 +156,21 @@ object Gen {
 
   def const[T](t: T): Gen[T] = fromOption(Some(t))
 
-  def fromOption[T](o: Option[T]): Gen[T] = gen(_ => res(o))
+  def fromOption[T](o: Option[T]): Gen[T] = gen(p => res(p.seed, o))
 
   def parameterized[T](f: Parameters => Gen[T]): Gen[T] = gen(p => f(p).apply(p))
 
   val size: Gen[Int] = parameterized(p => const(p.size))
 
-  val seed: Gen[Long] = parameterized(p => const(p.seed))
+  val long: Gen[Long] = gen { p =>
+    val (r, nextSeed) = Rng.long(p.seed)
+    res(nextSeed, Some(r))
+  }
 
-  def seed(mod: Long): Gen[Long] = seed map (_ % mod)
+  val double: Gen[Double] = gen { p =>
+    val (r, nextSeed) = Rng.double(p.seed)
+    res(nextSeed, Some(r))
+  }
 
   def zip[T1,T2](g1: Gen[T1], g2: Gen[T2]
   ): Gen[(T1,T2)] = g1.flatMap(t1 => g2.map((t1,_)))
@@ -203,44 +189,40 @@ object Gen {
     g5.map((t._1,t._2,t._3,t._4,_))
   )
 
-  private def dropIdx[T](xs: Traversable[T], idx: Int): Seq[T] = {
+  private def dropIdx[T](xs: Traversable[T], idx: Int): (T,Seq[T]) = {
     val b = new collection.immutable.VectorBuilder[T]
     var i = 0
-    xs.foreach { x => if (i != idx) { b += x }; i += 1 }
-    b.result
+    var t: Option[T] = None
+    xs.foreach { x =>
+      if (i != idx) b += x
+      else t = Some(x)
+      i += 1
+    }
+    (t.get, b.result)
   }
 
   def oneOfGens[T](gs: Seq[Gen[T]]): Gen[T] =
     if (gs.isEmpty) fail
-    else gen { prms =>
-      val idx = (math.abs(prms.seed) % gs.size).toInt
-      val rest = dropIdx(gs, idx)
-      val r = gs(idx).apply(prms)
-      new Result[T] {
-        val params = prms
-        val value = r.value
-        def shrink = r.shrink
-        def next = (r.next,rest.isEmpty) match {
-          case (g,true) => g
-          case (None,false) => Some(oneOfGens(rest))
-          case (Some(g),false) => Some(oneOfGens(g +: rest))
-        }
+    else long flatMap { n =>
+      val idx = (math.abs(n) % gs.size).toInt
+      val (g, rest) = dropIdx(gs, idx)
+      if(rest.isEmpty) g
+      else g mapNext {
+        case None => Some(oneOfGens(rest))
+        case Some(g0) => Some(oneOfGens(g0 +: rest))
       }
     }
 
   def oneOfGens[T](g0: Gen[T], gn: Gen[T]*): Gen[T] = oneOfGens(g0 +: gn)
 
-  def oneOf[T](xs: Seq[T]): Gen[T] =
-    if(xs.isEmpty) fail
-    else gen { prms =>
-      val idx = (math.abs(prms.seed) % xs.size).toInt
-      val rest = dropIdx(xs, idx)
-      new Result[T] {
-        val params = prms
-        val value = Some(xs(idx))
-        def shrink = xs.take(idx).reverse
-        def next = if(rest.isEmpty) None else Some(oneOf(rest))
-      }
+  def oneOf[T](ts: Seq[T]): Gen[T] =
+    if(ts.isEmpty) fail
+    else long flatMap { n =>
+      val idx = (math.abs(n) % ts.size).toInt
+      val (t, rest) = dropIdx(ts, idx)
+      const(t) setNextOpt {
+        if(rest.isEmpty) None else Some(oneOf(rest))
+      } setShrink { _ => ts.take(idx).reverse }
     }
 
   def oneOf[T](x0: T, xs: T*): Gen[T] = oneOf(x0 +: xs)
--- src/test/scala/org/scalacheck/GenSpecification.scala
+++ src/test/scala/org/scalacheck/GenSpecification.scala
@@ -9,21 +9,10 @@ object GenSpecification extends Properties("Gen") {
 
   val gens = oneOf(const(1), oneOf(2,3,4), const(0).repeatedly)
 
-  property("setSize") = forAll(gens) { g =>
-    forAll(oneOf(0,1,2,3)) { sz =>
-      g.setSize(sz).flatMap(_ => size).map(_ ?== sz)
-    }
+  property("resize") = forAll(oneOf(8,7,6,5)) { sz =>
+    size.resize(sz).map(_ ?== sz)
   }
 
-  property("setSeed") = forAll(gens) { g =>
-    forAll(oneOf(0,100,200)) { s =>
-      g.setSeed(s).flatMap(_ => seed).map(_ ?== s)
-    }
-  }
-
-  property("just testing") = for {
-    n <- oneOf(1,2,3)
-    m <- oneOf(5,6,7)
-  } yield boolToResult(n < m)
+  property("repeat") = forAll(const(0).repeatedly) { _ == 0 }
 
 }
